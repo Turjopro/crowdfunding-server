8f42db04101bd20a03683599be729d21396783b2
Turjopro <mdtatrimtafsir@gmail.com>
2026-09-05 14:07:38 +0600

    Connect server to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
- const express = require('express');
+const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,9 +9,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Crowdfunding server is running');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r6qfvfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('crowdfundingDB');
+    const usersCollection = db.collection('users');
+    const campaignsCollection = db.collection('campaigns');
+    const contributionsCollection = db.collection('contributions');
+    const withdrawalsCollection = db.collection('withdrawals');
+    const notificationsCollection = db.collection('notifications');
+    const paymentsCollection = db.collection('payments');
+    const reportsCollection = db.collection('reports');
+
+    app.get('/', (req, res) => {
+      res.send('Crowdfunding server is running');
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+run();
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
